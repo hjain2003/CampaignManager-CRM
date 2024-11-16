c1a2c553f1fcfea9f4fc23abff913e4e46fc4fb5
Harsh <hjain_be21@thapar.edu>
2024-11-16 17:53:30 +0530

    changed origin route

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ connectDB();
 //middlewares
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://campaign-manager-crm-frontend.vercel.app/',
+    origin: 'https://campaign-manager-crm-frontend.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
